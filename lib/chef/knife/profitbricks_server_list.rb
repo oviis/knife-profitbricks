1609d9fdaddc9187b6a3ddc679ed791719edd927
dto.isai <ovidiu.isai@dertouristik.com>
2014-11-07 11:28:30 +0100

    fixing the server list query

--- lib/chef/knife/profitbricks_server_list.rb
+++ lib/chef/knife/profitbricks_server_list.rb
@@ -30,27 +30,26 @@ class Chef
         #pp datacenters
 
         server_list = [
-            #ui.color('ID', :bold),
+            ui.color('ID', :bold),
             ui.color('Name', :bold),
             ui.color('DC', :bold),
             ui.color('CPUs', :bold),
             ui.color('RAM', :bold),
-            ui.color('Lan_ID', :bold),
-            ui.color('IPs', :bold)
-
+            ui.color('Lan_ID', :bold)
+            #ui.color('IPs', :bold)
         ]
+
 
         datacenters.each do |dc|
           dc.servers.each do |s|
             s.nics.each do |n|
-
-              #server_list << s.id
+              server_list << s.id
               server_list << s.name
               server_list << dc.name
               server_list << s.cores.to_s
               server_list << s.ram.to_s
               server_list << n.lan_id.to_s
-              server_list << n.ips.to_s
+              #server_list << n.ips.to_s
               #server_list << (n.respond_to?("ips") ? s.ips : "")
             end
           end
